42a32c2b449808ec7735245fc61fd337cfb05461
orcchieftain <karlos.slaj@gmail.com>
2024-03-10 18:21:47 +0100

    Svelte rendering loop {#each} added

--- src/routes/birthdays/+page.svelte
+++ src/routes/birthdays/+page.svelte
@@ -1,5 +1,27 @@
 <script>
 	import Birthday from '$lib/Birthday.svelte';
+
+	export let data;
+	console.log(data);
 </script>
 
-<Birthday></Birthday>
+<h1>Birthday list</h1>
+
+<ol>
+	{#each data.birthdays as birthday}
+		<li>
+			<Birthday {...birthday}></Birthday>
+		</li>
+	{/each}
+</ol>
+
+<style>
+	ol {
+		list-style-type: none;
+		padding-left: 0;
+	}
+
+	li {
+		padding: 10px;
+	}
+</style>
